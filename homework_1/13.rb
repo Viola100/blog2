447b4533b66d822c78819710724fa78b28dd93eb
Gabriela Borowa <gabrielapiekos92@gmail.com>
2018-10-11 00:55:16 +0200

    10.10.18

--- homework_1/13.rb
+++ homework_1/13.rb
@@ -1,13 +1,10 @@
+i = 0
 puts "podaj liczbę, żeby wyświetlić kolejne"
 n = gets.to_i
-i = 0
-loop do
+while n > 1
   i = i + 1
+  puts i
+end
 if n < 1
-  puts "liczba musi być większa od 1"
-  next
-end
-if i <= 100
-  break
-end
+  puts "Liczba musi być > 0"
 end
